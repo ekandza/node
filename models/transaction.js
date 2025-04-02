ec48dfde0d15eedef6b879cff448a26e41cf5dc4
ekandza <58695524+ekandza@users.noreply.github.com>
2025-04-02 05:52:49 -0700

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
-    produits: [{ produitId: String, quantite: Number }],
-    total: Number,
+    produits: [{
+        produitId: { type: mongoose.Schema.Types.ObjectId, ref: 'Produit', required: true },
+        quantite: { type: Number, required: true, min: 1 }
+    }],
+    total: { type: Number, required: true, min: 0 },
     date: { type: Date, default: Date.now }
 });
+
 const Transaction = mongoose.model('Transaction', TransactionSchema);
+module.exports = Transaction;
